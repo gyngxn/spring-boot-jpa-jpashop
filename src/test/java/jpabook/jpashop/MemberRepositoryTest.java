9d6a6db548f79e6bb99a77f406a3ca93fc57a965
gyngxn_ <jdj0311028@naver.com>
2025-09-08 21:49:11 +0900

    session 2

---
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,33 @@
+package jpabook.jpashop;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+public class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    @Transactional
+    @Rollback(false)
+    public void testMember() throws Exception {
+        //given
+        Member member = new Member();
+        member.setUsername("memberA");
+
+        //when
+        Long saveId = memberRepository.save(member);
+        Member findMember = memberRepository.find(saveId);
+
+        // then
+        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId()); // ID 확인
+        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername()); // 이름 확인
+        Assertions.assertThat(findMember).isEqualTo(member); // JPA 동일성 확인 (1차 캐시 때문에 동일 객체)
+    }
+}
