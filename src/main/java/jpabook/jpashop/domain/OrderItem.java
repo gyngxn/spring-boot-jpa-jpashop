7686fa48b7f9e4730347b43ca0e8e9b8ec051705
gyngxn_ <jdj0311028@naver.com>
2025-09-13 00:19:18 +0900

    session 7

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import jpabook.jpashop.domain.item.Item;
 import lombok.Getter;
 import lombok.Setter;
+import org.aspectj.weaver.ast.Or;
 
 @Entity
 @Table(name = "order_item")
@@ -24,4 +25,30 @@ public class OrderItem {
 
     private int orderPrice; //주문 가격
     private int count; // 주문 수량
+
+    //==생성 메서드==//
+    /**
+     * 상품, 가격, 수량들을 받아 새로운 주문상품을 만듦
+     */
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count); // 재고 줄이기
+        return orderItem;
+    }
+
+    //==비즈니스 로직==//
+    /** 주문 취소 */
+    public void cancel() {
+        getItem().addStock(count);
+    }
+
+    //==조회 로직==//
+    /** 주문상품 전체 가격 조회 */
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
 }
---
+++ src/main/java/jpabook/jpashop/service/OrderService.java
@@ -0,0 +1,70 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Delivery;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.repository.ItemRepository;
+import jpabook.jpashop.repository.MemberRepository;
+import jpabook.jpashop.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+    private final MemberRepository memberRepository;
+    private final ItemRepository itemRepository;
+
+    /** 주문 */
+    @Transactional
+    public Long order(Long memberId, Long itemId, int count) {
+
+        //엔티티 조회
+        Member member = memberRepository.findOne(memberId);
+        Item item = itemRepository.findOne(itemId);
+
+        //배송정보 조회
+        Delivery delivery = new Delivery();
+        delivery.setAddress(member.getAddress());
+
+        //주문상품 조회
+        OrderItem orderItem = OrderItem.createOrderItem(item, item.getPrice(), count);
+
+        // OrderItem orderItem1 = new OrderItem();
+        // orderItem1.setCount();
+        /**
+         * 생성 로직을 변경할 때 유지보수가 어렵다
+         * OrderItem 엔티티에서 protected 메서드를 만들어서 막는 방법도 있다
+         * Lombok 으로도 가능, NoArgumentConstructor(access = AccessLevel.PROTECTED)
+         */
+
+        //주문 생성
+        Order order = Order.createOrder(member, delivery, orderItem);
+
+        //주문 저장
+        orderRepository.save(order);
+        return order.getId();
+    }
+
+    /** 주문 취소 */
+    @Transactional
+    public void cacnelOrder(Long orderId) {
+
+        //주문 엔티티 조회
+        Order order = orderRepository.findOne(orderId);
+
+        //주문 취소
+        order.cancel();
+    }
+
+    //검색
+    //public List<Order> findOrders(OrderSearch orderSearch) {
+    //   return orderRepository.findAll(orderSearch);
+    //}
+}
